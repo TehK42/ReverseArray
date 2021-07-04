bdf7273dca8573c80c20731316c93394641d1249
Luigi <tupazluisgabriel@gmail.com>
2021-07-04 16:59:10 +0800

    Initial commit

---
+++ src/com/tehk42/Main.java
@@ -0,0 +1,11 @@
+package com.tehk42;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ReverseArray x = new ReverseArray();
+        int[] myArray = {1,2,3,4,5};
+        x.reverse(myArray);
+    }
+}
---
+++ src/com/tehk42/ReverseArray.java
@@ -0,0 +1,15 @@
+package com.tehk42;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
+        for(int i = 0; i < array.length/2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+        }
+        System.out.println("Reversed array = " + Arrays.toString(array));
+    }
+}
